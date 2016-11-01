ca7ce333d5f31dfc3cf7c253a796fbd2ba5223d2
Peter Khotpanya <pkhotpan@gmail.com>
2016-10-31 17:49:04 -0700

    Fixed orientation change for SentMemesDetailViewController and add image assets.
    -Added image assets for table, collection, and appIcon.
    -Fixed horizontal and vertical orientation for Detail View Controller.

--- MemeMe 2.0/SentMemesTableViewCell.swift
+++ MemeMe 2.0/SentMemesTableViewCell.swift
@@ -13,15 +13,4 @@ class SentMemesTableViewCell: UITableViewCell {
     @IBOutlet weak var sentMemesImageView: UIImageView!
     @IBOutlet weak var sentMemesLabel: UILabel!
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
