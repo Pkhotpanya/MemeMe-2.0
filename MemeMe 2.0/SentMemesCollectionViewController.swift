cb240176d33a0a12c8dda6bd14962697caa0474b
Peter Khotpanya <pkhotpan@gmail.com>
2016-10-31 17:05:59 -0700

    Configured UICollectionViewController
    -Set up shared memory model
    -Implemented collection view delegates numberOfSections,
    numberOfItemsInSection, and cellForItemAt
    -Added segue handling for SentMemesDetailViewSegue and
    MemeEditorViewSegue
    -Removed commented boilerplate code.

--- MemeMe 2.0/SentMemesCollectionViewController.swift
+++ MemeMe 2.0/SentMemesCollectionViewController.swift
@@ -12,17 +12,12 @@ private let reuseIdentifier = "Cell"
 
 class SentMemesCollectionViewController: UICollectionViewController {
 
-    var memes: [Meme] {
-        return (UIApplication.shared.delegate as! AppDelegate).memes
-    }
+    var memes: [Meme]!
     
     @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
 
         // Register cell classes
         self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
@@ -37,32 +32,21 @@ class SentMemesCollectionViewController: UICollectionViewController {
         flowLayout.minimumLineSpacing = space
         flowLayout.itemSize = CGSize(width: dimension, height: dimension)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        memes = appDelegate.memes
+        collectionView?.reloadData()
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
 
     // MARK: UICollectionViewDataSource
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
-
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
         return memes.count
     }
 
@@ -70,53 +54,29 @@ class SentMemesCollectionViewController: UICollectionViewController {
         // Configure the cell
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "sentMemesCollectionViewCell", for: indexPath) as! SentMemesCollectionViewCell
         let meme = memes[indexPath.item]
-        let imageView = UIImageView(image: meme.memeImage)
-        cell.backgroundView = imageView
+        cell.sentMemesImageView.image = meme.memeImage
  
         return cell
     }
 
-    // MARK: UICollectionViewDelegate
-    
-    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        // Grab the DetailVC from Storyboard
-        let object: AnyObject = self.storyboard!.instantiateViewController(withIdentifier: "SentMemesDetailViewController")
-        let detailVC = object as! SentMemesDetailViewController
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
         
-        //Populate view controller with data from the selected item
-        detailVC.sentMemesImageView = UIImageView(image: self.memes[indexPath.row].memeImage)
-        
-        // Present the view controller using navigation
-        navigationController!.pushViewController(detailVC, animated: true)
+        if  segue.identifier == "sentMemesDetailViewSegue" {
+            if let destination = segue.destination as? SentMemesDetailViewController{
+                if let indexPaths = collectionView?.indexPathsForSelectedItems {
+                    if let indexPath = indexPaths.first{
+                        let meme = memes[indexPath.item]
+                        destination.memeImage = meme.memeImage
+                    }
+                }
+            }
+            
+        } else if segue.identifier == "memeEditorViewSegue" {
+            
+        }
     }
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
 
 }
